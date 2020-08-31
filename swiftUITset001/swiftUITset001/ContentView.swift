497b01ed5396ca5d9f8526dcc7239dc0208d3d63
sunShine <super_hsh@163.com>
2020-09-01 00:09:15 +0800

    first Commit
    first commit

---
+++ swiftUITset001/swiftUITset001/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUITset001
+//
+//  Created by sunShine on 2020/8/31.
+//  Copyright © 2020 sunShine. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
