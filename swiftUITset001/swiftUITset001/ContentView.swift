ddb4a355d409ea04ca74d5562fc00b307e7e0ee8
sunShine <super_hsh@163.com>
2020-09-01 22:54:22 +0800

    class one finished

--- swiftUITset001/swiftUITset001/ContentView.swift
+++ swiftUITset001/swiftUITset001/ContentView.swift
@@ -10,9 +10,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        HStack() {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: false)
+            }
+        }
+        .font(Font.largeTitle)
+        .padding()
+        .foregroundColor(Color.orange)
+
     }
 }
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View {
+        ZStack() {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                        RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                        Text("👻")
+            }else{
+                 RoundedRectangle(cornerRadius: 10.0).fill(Color.orange)
+            }
+
+           
+        
+        }
+    }
+    
+}
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
