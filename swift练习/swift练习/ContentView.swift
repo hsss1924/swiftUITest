f2ad12b8f72c4d91d565c10ee9c11d9b60f8b17c
sunShine <super_hsh@163.com>
2020-09-13 22:53:10 +0800

    22

---
+++ swift练习/swift练习/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  swift练习
+//
+//  Created by sunShine on 2020/9/8.
+//  Copyright © 2020 sunShine. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
