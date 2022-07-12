e4c1c999504741beadaa3ed7fc2cba9ff01c6b61
韩帅 <super_hsh@163.com>
2022-07-12 17:04:20 +0800

    update

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var a = 1
+
+var x = 0.0, y = 2.0
+
+print(y)
+
+var name: String
+name = "dog"
+print(name)
+
+var red, green, blue: Double
+
+var friendName = "hello"
+friendName = "bonjour"
+print(friendName,terminator: "")
+
+print("hello new world is \(friendName)")
+
+// 注释
+
+let cat = "catt"
+print(cat)
+
+let minvalue = Int8.min
+print(minvalue)
