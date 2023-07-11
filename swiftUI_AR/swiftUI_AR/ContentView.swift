8b6b69e537c87dae2116907490ea6e1cec768dbf
sunShine <super_hsh@163.com>
2023-07-11 09:40:52 +0800

    update

---
+++ swiftUI_AR/swiftUI_AR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftUI_AR
+//
+//  Created by sunShine on 2023/7/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftUI_AR/swiftUI_AR/swiftUI_ARApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_ARApp.swift
+//  swiftUI_AR
+//
+//  Created by sunShine on 2023/7/11.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_ARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
